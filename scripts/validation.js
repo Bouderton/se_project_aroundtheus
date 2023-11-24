525840313ee8aff059bb75e9584b60a73672763a
bouderton <rmkhazal@gmail.com>
2023-11-24 13:48:22 -0800

    form validation bugs

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,22 +1,22 @@
-function showInputError(forms, inputElements, { inputErrorClass }) {
-  const errorMessageEl = forms.querySelector(`#${inputElements.id}-error`);
-  inputElements.classList.add(".modal__error");
-  errorMessageEl.textContent = inputElements.validationMessage;
-  inputElements.classList.add(inputErrorClass);
+function showInputError(forms, inputElement, { inputErrorClass }, errorClass) {
+  const errorMessageEl = forms.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add("modal__form-input_type_error");
+  errorMessageEl.textContent = inputElement.validationMessage;
+  errorMessageEl.classList.add(errorClass);
 }
 
-function hideInputError(forms, inputElements, { inputErrorClass }) {
-  const errorMessageEl = forms.querySelector(`#${inputElements.id}-error`);
-  inputElements.classList.remove(".modal__error");
-  inputElements.classList.remove(inputErrorClass);
+function hideInputError(forms, inputElement, { inputErrorClass }) {
+  const errorMessageEl = forms.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove("modal__form-input_type_error");
+  inputElement.classList.remove(inputErrorClass);
   errorMessageEl.textContent = "";
 }
 
-function checkInputValidity(forms, inputElements) {
-  if (!inputElements.validity.valid) {
-    showInputError(forms, inputElements, config);
+function checkInputValidity(forms, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(forms, inputElement, config);
   } else {
-    hideInputError(forms, inputElements, config);
+    hideInputError(forms, inputElement, config);
   }
   profileEditBtn.addEventListener("click", () => {
     profileTitleInput.value = profileTitle.textContent;
@@ -26,14 +26,22 @@ function checkInputValidity(forms, inputElements) {
     );
     openPopup(profileEditModal);
   });
+  addNewCardBtn.addEventListener("click", () => {
+    cardTitleInput.value = "";
+    cardUrlInput.value = "";
+    [cardTitleInput, cardUrlInput].forEach((input) => {
+      checkInputValidity(input);
+    });
+    openPopup(addCardForm);
+  });
 }
 
 function setEventListeners(forms, config) {
   const { inputSelector } = config;
   const inputElements = forms.querySelectorAll(inputSelector);
-  inputElements.forEach((inputElements) => {
-    inputElements.addEventListener("input", (e) => {
-      checkInputValidity(forms, inputElements, config);
+  inputElements.forEach((inputElement) => {
+    inputElement.addEventListener("input", (e) => {
+      checkInputValidity(forms, inputElement, config);
     });
   });
 }
@@ -53,9 +61,9 @@ const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__form-input",
   submitButtonSelector: ".modal__save-button",
-  inactiveButtonClass: "modal__button_disabled",
+  inactiveButtonClass: ".modal__button_disabled",
   inputErrorClass: ".modal__error",
-  errorClass: "modal__error_visible",
+  errorClass: ".modal__form-input_type_error",
 };
 
 enableValidation(config);
