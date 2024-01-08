3cf11c86f3075f741caa245bd2857265d98cb4c9
bouderton <rmkhazal@gmail.com>
2024-01-07 21:01:25 -0800

    webpack installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const path = require("path");
 
 module.exports = {
+  devtool: "inline-source-map",
   entry: {
     main: "./src/index.js",
+    stats: "errors-only",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
     publicPath: "",
+  },
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+    liveReload: true,
+    hot: false,
   },
 };
