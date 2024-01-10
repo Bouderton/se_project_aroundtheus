088dfb1d96235671c1003ddc9e7452c768ef5443
bouderton <rmkhazal@gmail.com>
2024-01-10 14:41:02 -0800

    popup classes start

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,20 @@
+import Popup from "./Popup.js";
+
+class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super({ popupSelector });
+    this._popupForm = this._popupElement.querySelector(".modal-form");
+    this._handleFormSubmit = handleFormSubmit;
+  }
+
+  close() {
+    this._popupForm.reset();
+    super.close();
+  }
+}
+
+//index.js
+
+const formPopup = new PopupWithForm("#profile-edit-modal", () => {});
+formPopup.open();
+formPopup.close();
